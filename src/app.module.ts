aa20cda1ee4e46bf8b4ad9878cd0f6c1421fc5b0
chake-vv <chake-vv@gmail.com>
2021-02-05 00:29:40 +0900

    graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: false,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
